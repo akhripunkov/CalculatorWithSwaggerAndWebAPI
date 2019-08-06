4a61770d88ada639a38d44d6e2de721716b2a0aa
Alexander <Alexander-e-mail@yandex.ru>
2019-08-06 10:44:17 +0300

    finally swagger added

--- SwaggerAddWebApi/Startup.cs
+++ SwaggerAddWebApi/Startup.cs
@@ -52,6 +52,7 @@ namespace SwaggerAddWebApi
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
             });
 
             app.UseHttpsRedirection();
